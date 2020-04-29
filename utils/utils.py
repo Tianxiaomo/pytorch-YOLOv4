281d100a94bc5815f730c8a6f98d492677c76499
Tianxiaomo <1025568329@qq.com>
2020-04-29 22:29:35 +0800

    del some mothod

--- utils/utils.py
+++ utils/utils.py
@@ -12,16 +12,6 @@ import struct  # get_image_size
 import imghdr  # get_image_size
 
 
-def sigmoid(x):
-    return 1.0 / (math.exp(-x) + 1.)
-
-
-def softmax(x):
-    x = torch.exp(x - torch.max(x))
-    x = x / x.sum()
-    return x
-
-
 def bbox_iou(box1, box2, x1y1x2y2=True):
     if x1y1x2y2:
         mx = min(box1[0], box2[0])
