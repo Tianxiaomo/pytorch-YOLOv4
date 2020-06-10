e9709dbf1611e90a495f15dc2a26e6355f7507b9
ersheng <ersheng@nvidia.com>
2020-06-10 20:10:50 +0800

    yolo_forward update

--- tool/torch_utils.py
+++ tool/torch_utils.py
@@ -76,6 +76,108 @@ def convert2cpu_long(gpu_matrix):
     return torch.LongTensor(gpu_matrix.size()).copy_(gpu_matrix)
 
 
+def yolo_forward_alternative(output, conf_thresh, num_classes, anchors, num_anchors, only_objectness=1,
+                              validation=False):
+    # Output would be invalid if it does not satisfy this assert
+    # assert (output.size(1) == (5 + num_classes) * num_anchors)
+
+    # print(output.size())
+
+    # Slice the second dimension (channel) of output into:
+    # [ 2, 2, 1, num_classes, 2, 2, 1, num_classes, 2, 2, 1, num_classes ]
+    # And then into
+    # bxy = [ 6 ] bwh = [ 6 ] det_conf = [ 3 ] cls_conf = [ num_classes * 3 ]
+    batch = output.size(0)
+    H = output.size(2)
+    W = output.size(3)
+
+    device = None
+    cuda_check = output.is_cuda
+    if cuda_check:
+        device = output.get_device()
+
+
+    # Prepare C-x, C-y, P-w, P-h (None of them are torch related)
+    grid_x = np.expand_dims(np.linspace(0, W - 1, W), axis=0).repeat(H, 0).reshape(1, 1, H * W).repeat(batch, 0).repeat(num_anchors, 1)
+    grid_y = np.expand_dims(np.linspace(0, H - 1, H), axis=1).repeat(W, 1).reshape(1, 1, H * W).repeat(batch, 0).repeat(num_anchors, 1)
+    # Shape: [batch, num_anchors, H * W]
+    grid_x_tensor = torch.tensor(grid_x, device=device, dtype=torch.float32)
+    grid_y_tensor = torch.tensor(grid_y, device=device, dtype=torch.float32)
+
+    anchor_array = np.array(anchors).reshape(1, num_anchors, 2)
+    anchor_array = anchor_array.repeat(batch, 0)
+    anchor_array = np.expand_dims(anchor_array, axis=3).repeat(H * W, 3)
+    # Shape: [batch, num_anchors, 2, H * W]
+    anchor_tensor = torch.tensor(anchor_array, device=device, dtype=torch.float32)
+
+    # normalize coordinates to [0, 1]
+    normal_array = np.array([1.0 / W, 1.0 / H, 1.0 / W, 1.0 / H], dtype=np.float32).reshape(1, 1, 4)
+    normal_array = normal_array.repeat(batch, 0)
+    normal_array = normal_array.repeat(num_anchors * H * W, 1)
+    # Shape: [batch, num_anchors * H * W, 4]
+    normal_tensor = torch.tensor(normal_array, device=device, dtype=torch.float32)
+
+    bxy_list = []
+    bwh_list = []
+    det_confs_list = []
+    cls_confs_list = []
+
+    for i in range(num_anchors):
+        begin = i * (5 + num_classes)
+        end = (i + 1) * (5 + num_classes)
+        
+        bxy_list.append(output[:, begin : begin + 2])
+        bwh_list.append(output[:, begin + 2 : begin + 4])
+        det_confs_list.append(output[:, begin + 4 : begin + 5])
+        cls_confs_list.append(output[:, begin + 5 : end])
+
+    # Shape: [batch, num_anchors * 2, H, W]
+    bxy = torch.cat(bxy_list, dim=1)
+    # Shape: [batch, num_anchors * 2, H, W]
+    bwh = torch.cat(bwh_list, dim=1)
+
+    # Shape: [batch, num_anchors, H, W]
+    det_confs = torch.cat(det_confs_list, dim=1)
+    # Shape: [batch, num_anchors * H * W]
+    det_confs = det_confs.view(batch, num_anchors * H * W)
+
+    # Shape: [batch, num_anchors * num_classes, H, W]
+    cls_confs = torch.cat(cls_confs_list, dim=1)
+    # Shape: [batch, num_anchors, num_classes, H * W]
+    cls_confs = cls_confs.view(batch, num_anchors, num_classes, H * W)
+    # Shape: [batch, num_anchors, num_classes, H * W] --> [batch, num_anchors * H * W, num_classes] 
+    cls_confs = cls_confs.permute(0, 1, 3, 2).reshape(batch, num_anchors * H * W, num_classes)
+
+    # Apply sigmoid(), exp() and softmax() to slices
+    #
+    bxy = torch.sigmoid(bxy)
+    bwh = torch.exp(bwh)
+    det_confs = torch.sigmoid(det_confs)
+    cls_confs = torch.nn.Softmax(dim=2)(cls_confs)
+
+    # Shape: [batch, num_anchors, 2, H * W]
+    bxy = bxy.view(batch, num_anchors, 2, H * W)
+    # Shape: [batch, num_anchors, 2, H * W]
+    bwh = bwh.view(batch, num_anchors, 2, H * W)
+
+    # Apply C-x, C-y, P-w, P-h
+    bxy[:, :, 0] += grid_x_tensor
+    bxy[:, :, 1] += grid_y_tensor
+
+    print(anchor_tensor.size())
+    bwh *= anchor_tensor
+
+    # Shape: [batch, num_anchors, 4, H * W] --> [batch, num_anchors * H * W, 4]
+    boxes = torch.cat((bxy, bwh), dim=2).permute(0, 1, 3, 2).reshape(batch, num_anchors * H * W, 4)
+
+    print(normal_tensor.size())
+    boxes *= normal_tensor
+
+
+    return  boxes, cls_confs, det_confs
+
+
+
 def yolo_forward(output, conf_thresh, num_classes, anchors, num_anchors, only_objectness=1,
                               validation=False):
     # Output would be invalid if it does not satisfy this assert
@@ -84,41 +186,52 @@ def yolo_forward(output, conf_thresh, num_classes, anchors, num_anchors, only_ob
     # print(output.size())
 
     # Slice the second dimension (channel) of output into:
-    # [ 1, 1, 1, 1, 1, num_classes, 1, 1, 1, 1, 1, num_classes, 1, 1, 1, 1, 1, num_classes ]
-    #
-    list_of_slices = []
+    # [ 2, 2, 1, num_classes, 2, 2, 1, num_classes, 2, 2, 1, num_classes ]
+    # And then into
+    # bxy = [ 6 ] bwh = [ 6 ] det_conf = [ 3 ] cls_conf = [ num_classes * 3 ]
+    batch = output.size(0)
+    H = output.size(2)
+    W = output.size(3)
+
+    bxy_list = []
+    bwh_list = []
+    det_confs_list = []
+    cls_confs_list = []
 
     for i in range(num_anchors):
         begin = i * (5 + num_classes)
         end = (i + 1) * (5 + num_classes)
         
-        for j in range(5):
-            list_of_slices.append(output[:, begin + j : begin + j + 1])
+        bxy_list.append(output[:, begin : begin + 2])
+        bwh_list.append(output[:, begin + 2 : begin + 4])
+        det_confs_list.append(output[:, begin + 4 : begin + 5])
+        cls_confs_list.append(output[:, begin + 5 : end])
 
-        list_of_slices.append(output[:, begin + 5 : end])
+    # Shape: [batch, num_anchors * 2, H, W]
+    bxy = torch.cat(bxy_list, dim=1)
+    # Shape: [batch, num_anchors * 2, H, W]
+    bwh = torch.cat(bwh_list, dim=1)
+
+    # Shape: [batch, num_anchors, H, W]
+    det_confs = torch.cat(det_confs_list, dim=1)
+    # Shape: [batch, num_anchors * H * W]
+    det_confs = det_confs.view(batch, num_anchors * H * W)
+
+    # Shape: [batch, num_anchors * num_classes, H, W]
+    cls_confs = torch.cat(cls_confs_list, dim=1)
+    # Shape: [batch, num_anchors, num_classes, H * W]
+    cls_confs = cls_confs.view(batch, num_anchors, num_classes, H * W)
+    # Shape: [batch, num_anchors, num_classes, H * W] --> [batch, num_anchors * H * W, num_classes] 
+    cls_confs = cls_confs.permute(0, 1, 3, 2).reshape(batch, num_anchors * H * W, num_classes)
 
     # Apply sigmoid(), exp() and softmax() to slices
-    # [ 1, 1,     1, 1,     1,      num_classes, 1, 1, 1, 1, 1, num_classes, 1, 1, 1, 1, 1, num_classes ]
-    #   sigmid()  exp()  sigmoid()  softmax()
-    for i in range(num_anchors):
-        begin = i * (5 + 1)
-
-        # print(list_of_slices[begin].size())
-        
-        list_of_slices[begin] = torch.sigmoid(list_of_slices[begin])
-        list_of_slices[begin + 1] = torch.sigmoid(list_of_slices[begin + 1])
-        
-        list_of_slices[begin + 2] = torch.exp(list_of_slices[begin + 2])
-        list_of_slices[begin + 3] = torch.exp(list_of_slices[begin + 3])
-
-        list_of_slices[begin + 4] = torch.sigmoid(list_of_slices[begin + 4])
-
-        list_of_slices[begin + 5] = torch.nn.Softmax(dim=1)(list_of_slices[begin + 5])
+    #
+    bxy = torch.sigmoid(bxy)
+    bwh = torch.exp(bwh)
+    det_confs = torch.sigmoid(det_confs)
+    cls_confs = torch.nn.Softmax(dim=2)(cls_confs)
 
     # Prepare C-x, C-y, P-w, P-h (None of them are torch related)
-    batch = output.size(0)
-    H = output.size(2)
-    W = output.size(3)
     grid_x = np.expand_dims(np.expand_dims(np.expand_dims(np.linspace(0, W - 1, W), axis=0).repeat(H, 0), axis=0), axis=0)
     grid_y = np.expand_dims(np.expand_dims(np.expand_dims(np.linspace(0, H - 1, H), axis=1).repeat(W, 1), axis=0), axis=0)
 
@@ -133,50 +246,40 @@ def yolo_forward(output, conf_thresh, num_classes, anchors, num_anchors, only_ob
     if cuda_check:
         device = output.get_device()
 
-    # Apply C-x, C-y, P-w, P-h to slices
-    for i in range(num_anchors):
-        begin = i * (5 + 1)
-        
-        list_of_slices[begin] += torch.tensor(grid_x, device=device)
-        list_of_slices[begin + 1] += torch.tensor(grid_y, device=device)
-        
-        list_of_slices[begin + 2] *= anchor_w[i]
-        list_of_slices[begin + 3] *= anchor_h[i]
-
-
-    ########################################
-    #   Figure out bboxes from slices     #
-    ########################################
-
     bx_list = []
     by_list = []
     bw_list = []
     bh_list = []
 
-    det_confs_list = []
-    cls_confs_list = []
-
+    # Apply C-x, C-y, P-w, P-h
     for i in range(num_anchors):
-        begin = i * (5 + 1)
-
-        bx_list.append(list_of_slices[begin])
-        by_list.append(list_of_slices[begin + 1])
-        bw_list.append(list_of_slices[begin + 2])
-        bh_list.append(list_of_slices[begin + 3])
-
+        ii = i * 2
         # Shape: [batch, 1, H, W]
-        det_confs = list_of_slices[begin + 4]
+        bx = bxy[:, ii] + torch.tensor(grid_x, device=device, dtype=torch.float32)
+        # Shape: [batch, 1, H, W]
+        by = bxy[:, ii + 1] + torch.tensor(grid_y, device=device, dtype=torch.float32)
+        # Shape: [batch, 1, H, W]
+        bw = bwh[:, ii] * anchor_w[i]
+        # Shape: [batch, 1, H, W]
+        bh = bwh[:, ii + 1] * anchor_h[i]
 
-        # Shape: [batch, num_classes, H, W]
-        cls_confs = list_of_slices[begin + 5]
+        bx_list.append(bx)
+        by_list.append(by)
+        bw_list.append(bw)
+        bh_list.append(bh)
 
-        det_confs_list.append(det_confs)
-        cls_confs_list.append(cls_confs)
+
+    ########################################
+    #   Figure out bboxes from slices     #
+    ########################################
     
     # Shape: [batch, num_anchors, H, W]
     bx = torch.cat(bx_list, dim=1)
+    # Shape: [batch, num_anchors, H, W]
     by = torch.cat(by_list, dim=1)
+    # Shape: [batch, num_anchors, H, W]
     bw = torch.cat(bw_list, dim=1)
+    # Shape: [batch, num_anchors, H, W]
     bh = torch.cat(bh_list, dim=1)
 
     # normalize coordinates to [0, 1]
@@ -184,14 +287,6 @@ def yolo_forward(output, conf_thresh, num_classes, anchors, num_anchors, only_ob
     by = by / H
     bw = bw / W
     bh = bh / H
-
-    # Shape: [batch, num_anchors * H * W] 
-    det_confs = torch.cat(det_confs_list, dim=1).view(batch, num_anchors * H * W)
-
-    # Shape: [batch, num_anchors, num_classes, H * W] 
-    cls_confs = torch.cat(cls_confs_list, dim=1).view(batch, num_anchors, num_classes, H * W)
-    # Shape: [batch, num_anchors * H * W, num_classes] 
-    cls_confs = cls_confs.permute(0, 1, 3, 2).reshape(batch, num_anchors * H * W, num_classes)
 
     # Shape: [batch, num_anchors * H * W, 1]
     bx = bx.view(batch, num_anchors * H * W, 1)
@@ -204,9 +299,10 @@ def yolo_forward(output, conf_thresh, num_classes, anchors, num_anchors, only_ob
 
     # Shape: [batch, num_anchors * h * w, num_classes, 4]
     # boxes = boxes.view(N, num_anchors * H * W, 1, 4).expand(N, num_anchors * H * W, num_classes, 4)
-    
+
 
     return  boxes, cls_confs, det_confs
+
 
 
 
